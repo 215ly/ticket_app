da775e8b470c5452ea14140ddffde64e39400d03
gezi <2875527216@qq.com>
2024-06-19 18:22:43 +0800

    bottom_nav_bar.dart update and appStyle、appMedia Class ;add home_screens.dart

---
+++ lib/base/res/styles/app_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF687DAF);
+
+class AppStyles {
+  static Color primaryColor = primary;
+  static const Color textColor =  Color(0XFF3B3B3B);
+  static Color bgColor = const Color(0xFFEEEDF2);
+  static TextStyle headLineStyle1 =
+      const TextStyle(fontSize: 17, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle2 =
+      const TextStyle(fontSize: 26, fontWeight: FontWeight.bold,color: textColor);
+}
