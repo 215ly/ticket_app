08ed1d3e5eb3a41c33c57a8eab9d6191adca677e
gezi <2875527216@qq.com>
2024-06-20 18:07:04 +0800

    显示所有机票信息页面

---
+++ lib/base/utils/all.json.dart
@@ -0,0 +1,82 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': "PVG", 'name': '上海'},
+    'to': {'code': "FRA", 'name': "法兰克福"},
+    'flying_time': '11H 15M',
+    'date': "3 MAY",
+    'departure_time': "10:30 AM",
+    'number': 567
+  },
+  {
+    'from': {'code': "CAN", 'name': '广州'},
+    'to': {'code': "CDG", 'name': "巴黎"},
+    'flying_time': '13H 20M',
+    'date': "5 MAY",
+    'departure_time': "09:45 AM",
+    'number': 890
+  },
+  {
+    'from': {'code': "PEK", 'name': '北京'},
+    'to': {'code': "JFK", 'name': "纽约"},
+    'flying_time': '14H 50M',
+    'date': "7 MAY",
+    'departure_time': "11:00 AM",
+    'number': 123
+  },
+  {
+    'from': {'code': "SZX", 'name': '深圳'},
+    'to': {'code': "LAX", 'name': "洛杉矶"},
+    'flying_time': '13H 10M',
+    'date': "10 MAY",
+    'departure_time': "12:15 PM",
+    'number': 456
+  },
+  {
+    'from': {'code': "CTU", 'name': '成都'},
+    'to': {'code': "HND", 'name': "东京"},
+    'flying_time': '6H 30M',
+    'date': "12 MAY",
+    'departure_time': "02:00 PM",
+    'number': 789
+  },
+  {
+    'from': {'code': "XIY", 'name': '西安'},
+    'to': {'code': "ICN", 'name': "首尔"},
+    'flying_time': '3H 40M',
+    'date': "14 MAY",
+    'departure_time': "03:30 PM",
+    'number': 234
+  },
+  {
+    'from': {'code': "CKG", 'name': '重庆'},
+    'to': {'code': "SIN", 'name': "新加坡"},
+    'flying_time': '5H 20M',
+    'date': "16 MAY",
+    'departure_time': "04:45 PM",
+    'number': 678
+  },
+  {
+    'from': {'code': "XMN", 'name': '厦门'},
+    'to': {'code': "KIX", 'name': "大阪"},
+    'flying_time': '3H 15M',
+    'date': "18 MAY",
+    'departure_time': "05:00 PM",
+    'number': 901
+  },
+  {
+    'from': {'code': "HGH", 'name': '杭州'},
+    'to': {'code': "MEL", 'name': "墨尔本"},
+    'flying_time': '10H 55M',
+    'date': "20 MAY",
+    'departure_time': "06:30 PM",
+    'number': 345
+  },
+  {
+    'from': {'code': "WUH", 'name': '武汉'},
+    'to': {'code': "BCN", 'name': "巴塞罗那"},
+    'flying_time': '12H 05M',
+    'date': "22 MAY",
+    'departure_time': "07:45 PM",
+    'number': 678
+  }
+];
